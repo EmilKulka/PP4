961f4a28ea4ff3aa030ae77d3eea5e936dcf1381
s-301-29 <s-301-29@gumiforest.uek.krakow.pl>
2024-05-13 09:29:59 +0200

    Zajecia 13.05.24

---
+++ src/main/java/pl/ekulka/ecommerce/sales/ReservationDetail.java
@@ -0,0 +1,13 @@
+package pl.ekulka.ecommerce.sales;
+
+public class ReservationDetail {
+
+    public String getReservationId() {
+        return "123xyz";
+    }
+
+    public String getPaymentUrl() {
+        return "http://payment.dev/123xyz";
+    }
+
+}
--- src/main/java/pl/ekulka/ecommerce/sales/SalesController.java
+++ src/main/java/pl/ekulka/ecommerce/sales/SalesController.java
@@ -1,6 +1,8 @@
 package pl.ekulka.ecommerce.sales;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +18,19 @@ public class SalesController {
     Offer getCurrentOffer(){
         String customerId = getCurrentCustomerId();
         return sales.getCurrentOffer(customerId);
+    }
+
+    @PostMapping("/api/add-to-cart/{productId}")
+    void addToCart(@PathVariable String productId) {
+        String customerId = getCurrentCustomerId();
+        sales.addToCart(customerId, productId);
+    };
+
+    @PostMapping("api/accept-offer")
+    ReservationDetail acceptOffer(AcceptOfferRequest acceptOfferRequest) {
+        String customerId = getCurrentCustomerId();
+        ReservationDetail reservationDetail = sales.acceptOffer(customerId, acceptOfferRequest);
+        return  reservationDetail;
     }
 
     private String getCurrentCustomerId(){
